288ce9934d6b155924a7a2eb85774e5936d1500e
Hilary <hilary.koerner@gmail.com>
2021-07-06 20:10:49 -0500

    rsp elif

--- rpsloop.py
+++ rpsloop.py
@@ -16,33 +16,36 @@ while run=="y":
         print("you chose rock and the computer chose scissors")
         print("You win! Rock beats scissors.")
 
-    if user_choice == "p" and computer_choice == "r":
+    elif user_choice == "p" and computer_choice == "r":
         print("you chose paper and the computer chose rock")
         print("You win! Paper beats rock.")  
 
-    if user_choice == "s" and computer_choice == "p":
+    elif user_choice == "s" and computer_choice == "p":
         print("you chose scissors and the computer chose paper")
         print("You win! Scissors beats paper.")  
 
-    if user_choice == "r" and computer_choice == "p":
+    elif user_choice == "r" and computer_choice == "p":
         print("you chose rock and the computer chose paper")
         print("You lose! Paper beats rock.")
 
-    if user_choice == "p" and computer_choice == "s":
+    elif user_choice == "p" and computer_choice == "s":
         print("you chose paper and the computer chose scissors")
         print("You lose! Scissors beats paper.")  
 
-    if user_choice == "s" and computer_choice == "r":
+    elif user_choice == "s" and computer_choice == "r":
         print("you chose scissors and the computer chose rock")
         print("You lose! Rock beats scissors.") 
 
-    if user_choice == "r" and computer_choice == "r":
+    elif user_choice == "r" and computer_choice == "r":
         print("you both choose rock. Tie!")
 
-    if user_choice == "p" and computer_choice == "p":
+    elif user_choice == "p" and computer_choice == "p":
         print("you both choose paper. Tie!")
 
-    if user_choice == "s" and computer_choice == "s":
+    elif user_choice == "s" and computer_choice == "s":
         print("you both choose scissors. Tie!")
+
+    else: 
+        print("I dont understand your selection. Make your choice from 'r', 'p', 's'")
     
     run = input("would you like to play again 'y' : ")
